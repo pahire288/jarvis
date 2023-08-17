7e8298d6e345daa43c32f5d2b92385e17dedf450
pratik_ahire <pahire288@gmail.com>
2023-08-17 22:13:38 +0530

    QueryselectorAll

--- DOM/dom.js
+++ DOM/dom.js
@@ -70,3 +70,40 @@
 
 //QUERYSELECTOR//
 
+// var header = document.querySelector('#main-header');
+// header.computedStyleMap.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+
+// var item = document.querySelector('.list-group-item');
+// item.computedStyleMap.color = 'red';
+
+// var lastItem = Document.querySelector
+// ('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector
+// ('list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
+
+
+//Queryselectorall//
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].computedStyleMap.backgroundColor = '#f4f4f4';
+    even[i].computedStyleMap.backgroundColor = '#ccc';
+
+}
+
