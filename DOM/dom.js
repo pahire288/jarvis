8f5ff548acafefda1c85d5ba5c1fe596e9e947f6
pratik_ahire <pahire288@gmail.com>
2023-08-17 23:19:46 +0530

    Creating Node and Modifying Dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -92,18 +92,103 @@
 
 
 //Queryselectorall//
-var titles = document.querySelectorAll('.title');
+// var titles = document.querySelectorAll('.title');
 
-console.log(titles);
-titles[0].textContent = 'Hello';
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].computedStyleMap.backgroundColor = '#f4f4f4';
-    even[i].computedStyleMap.backgroundColor = '#ccc';
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].computedStyleMap.backgroundColor = '#f4f4f4';
+//     even[i].computedStyleMap.backgroundColor = '#ccc';
 
-}
+// }
 
+//TRAVERSING THE DOM //
+
+var itemList = document.querySelector('#items');
+// //parentNode
+// console.timeLog(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// //parentElement
+// console.timeLog(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+
+// 
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].computedStyleMap.backgroundColor = 'yellow';
+
+// //Firstchild
+
+// console.log(itemList.firstChild);
+// //FirstelementChild
+
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+
+//Lastchild
+
+// console.log(itemList.Lastchild);
+//lastelementChild
+
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 6';
+
+
+// //nexrsibilling
+// console.log(itemList.nextSibling);
+// //nextelementsibling //
+
+// console.log(itemList.nextElementSibling);
+
+//previoussibling
+
+// console.log(itemList.previousSibling);
+// //previouselementsibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.computedStyleMap.color = 'green';
+
+//createlement
+
+//create a div
+var nediv = document.createElement('div');
+
+//Add class
+rewdiv.className = 'hello';
+
+//add id
+
+newdiv.id = 'hello1';
+
+//Add attr
+newdiv.setAttribute('title','Hello div');
+
+//cretae textnode
+var newdivText = document.createTextNode('Hello eorld');
+
+
+//Add text to div
+newdiv.appendChild(newdivText);
+
+var container = document.querySelector('header.container');
+var h1 = document.querySelector('header h1');
+
+
+console.log(newdiv);
+
+newdiv.style.fontSize = '30px';
+
+container.insertBefore(newdiv,h1);;
